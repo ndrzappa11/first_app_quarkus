76b2e18346828b9c43a7151d9edaf87a80e7199f
quarkusio <no-reply@quarkus.io>
2022-06-07 12:40:49 +0000

    Initial commit

---
+++ src/test/java/org/zappa/GreetingResourceIT.java
@@ -0,0 +1,9 @@
+package org.zappa;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class GreetingResourceIT extends GreetingResourceTest {
+
+    // Execute the same tests but in native mode.
+}
